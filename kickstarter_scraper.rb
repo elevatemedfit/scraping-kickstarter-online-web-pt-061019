9319b601580638b710f5b0ae4bfb137014dbfc6d
Chris Junker <elevatemedfit@gmail.com>
2019-07-24 14:56:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,5 +4,8 @@ require 'pry'# require libraries/modules here
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
-kickstarter = Nokogiri::HTML(html)# write your code here
+kickstarter = Nokogiri::HTML(html)
+binding.pry # write your code here
 end
+
+create_project_hash
